c275d48ffafdb67aa8161fd9a38ec09d0da84c28
meathspace <40881591+MeathSpace@users.noreply.github.com>
2024-03-20 09:14:33 +0000

    Synced up branches

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import Datagrid from './components/Datagrid'
-import Datagrid2 from './components/Datagrid2'
-import Datagrid3 from './components/Datagrid3'
+import React from "react";
+import Datagrid from "./components/Datagrid";
+import Datagrid2 from "./components/Datagrid2";
+import Datagrid3 from "./components/Datagrid3";
+import Table from "./Table/Table";
 
 const App = () => {
   return (
     <>
-    <h1 style={{
-      textAlign:"center",
-      marginBlock:"25px"
-    }}>DataGrid Material UI</h1>
-    <Datagrid3/>
+      <h1
+        style={{
+          textAlign: "center",
+          marginBlock: "25px",
+        }}
+      >
+        DataGrid Material UI
+      </h1>
+      <Table />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
